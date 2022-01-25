379284f38692f9ff4a46869cf9e8d7ec89e729ea
Harshitha Alluri <harshitha.alluri@qentelli.com>
2022-01-25 17:31:56 +0530

    server side

---
+++ server.js
@@ -0,0 +1,22 @@
+const express= require('express')    
+
+const app=express()
+
+
+
+app.get('/',(req,res)=>{
+
+    res.send('Hello World')
+
+})
+
+
+const PORT=3000
+
+
+
+app.listen(PORT,()=>{
+
+    console.log(`server running at port ${PORT}`)
+
+})
