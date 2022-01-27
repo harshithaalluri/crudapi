dc7e0f745bc71a0d13d8e30a7f8f336d3b46dd4e
Harshitha Alluri <harshitha.alluri@qentelli.com>
2022-01-27 15:22:09 +0530

    initail comment

--- server.js
+++ server.js
@@ -1,15 +1,31 @@
+//import expressjs for creating the server and creating api routes
+const express= require('express') 
 
-const express= require('express')  
+
 // Body parsers for getting the data thrugh urls 
+const bodyParser=require('body-parser')
 
-const bodyparser=require('body-parser')
+
+// Importing Mongo client
+const MongoClient=require('mongodb').MongoClient
+
+
 // Const app controls the entire app with express functional constructor
 const app=express()
 
 // We are saying expressjs that to use body parser urlencoded to be parsed
-app.use(bodyparser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({extended:true}))
 
-// Create Route with Creating the quote
+// Datatbase Connection string
+const connectionString="mongodb+srv://harshithaalluri:Alluri1234@cluster0.chyl9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+//Connecting the database
+MongoClient.connect(connectionString,(err,client)=>{
+    if(err) return console.err(err)
+    
+    console.log('connected to database')
+})
+
+// Create Route with Creating the quote.two parameters 1.route 2.function
 app.post('/quotes',(req,res)=>
 {
    res.send(req.body)
@@ -25,4 +41,5 @@ app.listen(PORT,()=>{
 
     console.log(`server running at port ${PORT}`)
 
-})
+}) 
+
