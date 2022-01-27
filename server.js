ca9b15bf312897e04602cf3f98f5976ed178c38c
Harshitha Alluri <harshitha.alluri@qentelli.com>
2022-01-27 11:57:32 +0530

    html and sending quotes

--- server.js
+++ server.js
@@ -1,19 +1,25 @@
-const express= require('express')    
 
+const express= require('express')  
+// Body parsers for getting the data thrugh urls 
+
+const bodyparser=require('body-parser')
+// Const app controls the entire app with express functional constructor
 const app=express()
 
+// We are saying expressjs that to use body parser urlencoded to be parsed
+app.use(bodyparser.urlencoded({extended:true}))
 
-
-app.get('/',(req,res)=>{
-
-    res.send('Hello World')
-
+// Create Route with Creating the quote
+app.post('/quotes',(req,res)=>
+{
+   res.send(req.body)
 })
 
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname +'/index.html')
+})
 
 const PORT=3000
-
-
 
 app.listen(PORT,()=>{
 
